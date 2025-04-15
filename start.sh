72ef45a32f84e821098025cc88672f28dbcb35eb
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-04-15 13:30:43 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Télécharger Stockfish statique compatible Render
-mkdir -p stockfish
-curl -L -o stockfish/stockfish https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-ubuntu-x86-64
-chmod +x stockfish/stockfish
+# Installer stockfish depuis les dépôts
+apt-get update && apt-get install -y stockfish
 
 # Lancer l'app Flask
 gunicorn app:app
