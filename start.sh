3f97bb83da33a462182341359e6cefd9a111df53
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-04-15 13:00:52 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Télécharger Stockfish (AVX2 version Linux)
-mkdir -p stockfish
-curl -L -o stockfish/stockfish https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-ubuntu-x86-64-avx2
-chmod +x stockfish/stockfish
-
-# Lancer l'app Flask
-gunicorn app:app
