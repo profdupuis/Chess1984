526a30dd5486528da2de49d1afdd9b82c67b9219
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-04-15 13:23:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Télécharger Stockfish (AVX2 version Linux)
+# Télécharger Stockfish Linux compatible Render (x86-64-modern)
 mkdir -p stockfish
-curl -L -o stockfish/stockfish https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-linux-x86-64-avx2
+curl -L -o stockfish/stockfish https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-linux-x86-64-modern
 chmod +x stockfish/stockfish
 
 # Lancer l'app Flask
